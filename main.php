247181889af35756e6e7e5c24c716d07064ff25f
lawecal <guillaume.delamusse@gmail.com>
2015-03-10 16:01:20 +0100

    Premier commit depuis le Poste

---
+++ main.php
@@ -0,0 +1,32 @@
+<?php
+
+// Début du projet MasterFlux
+
+echo "Bonjour les gens <br/>";
+
+if(==){
+  echo "";
+}
+else {
+  echo "";
+}
+
+switch (){
+  case 1 :
+  echo "";
+  break;
+}
+
+function stop_meincraft ($longueur, $largeur)
+{
+  $aire = $longueur  * $largeur;
+
+  return $aire;
+}
+
+
+$test = aire(5,4);
+
+echo $test;
+
+?>
